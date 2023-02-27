e7c13af4334a759626b9c3fb7047850114756a95
Anjali Raj <92433906+devanjaliraj@users.noreply.github.com>
2023-02-27 19:40:18 +0530

    build 27/02/2023

--- build/static/js/25.620b9e18.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[25],{1395:function(a,s,d){}}]);
-//# sourceMappingURL=25.620b9e18.chunk.js.map
--- build/static/js/9.2be5af1f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[9],{974:function(a,s,d){}}]);
-//# sourceMappingURL=9.2be5af1f.chunk.js.map
